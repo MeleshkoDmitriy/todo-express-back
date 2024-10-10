31694a74e0d89e8647f88a8792d5cbc249d1760c
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-10-10 12:34:32 +0300

    updateTask

--- controllers/todos.js
+++ controllers/todos.js
@@ -126,7 +126,7 @@ const updateTask = async (req, res) => {
       .json({ message: "Задача не найдена" });
   }
 
-  const updatedTask = { ...foundTask, ...newTask };
+  const updatedTask = { ...foundTask, ...req.body };
   const index = todolist.indexOf(foundTask);
   todolist[index] = updatedTask;
   res.status(HTTP_STATUSES.OK_200).json(updatedTask);
